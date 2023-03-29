05db635925d6fada9c8a2972c7e40b9d8e80f9c9
Noman Atique <noman.atique@keyin.com>
2023-03-29 14:52:33 -0400

    march29

--- Week10/server.js
+++ Week10/server.js
@@ -1,30 +1,34 @@
+const { check, validationResult } = require("express-validator");
 const express = require("express");
 const path = require("path");
 
 var myApp = express();
 myApp.use(express.static("static"));
+myApp.use(express.urlencoded({ extended: false }));
 myApp.set("views", path.join(__dirname, "views"));
 myApp.set("view engine", "ejs");
 
+myApp.post(
+  "/",
+  [
+    check("name", "Must have a name").notEmpty(),
+    check("email", "Must haeve email").isEmail(),
+  ],
+  function (req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+  }
+);
+
 myApp.get("/", function (req, res) {
-  // res.sendFile(path.join(__dirname, "/pages/index.html"));
   res.render("home");
+});
+myApp.get("/about", function (req, res) {
+  res.render("about");
 });
 
 myApp.get("/contact", function (req, res) {
-  res.sendFile(path.join(__dirname, "/pages/contact.html"));
-});
-
-myApp.get("/about", function (req, res) {
-  res.sendFile(path.join(__dirname, "/pages/about.html"));
-});
-
-myApp.get("/help", function (req, res) {
-  res.sendFile(path.join(__dirname, "/pages/help.html"));
-});
-
-myApp.get("/history", function (req, res) {
-  res.sendFile(path.join(__dirname, "/pages/history.html"));
+  res.render("contact");
 });
 
 myApp.listen(8080);
